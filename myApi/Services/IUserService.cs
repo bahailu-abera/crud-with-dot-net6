51e87e899bdbf7d362c9dccad05acc941ff102a4
Bahailu Abera <aberabahailu4@gmail.com>
2023-04-12 11:10:09 +0300

    user service interface

---
+++ myApi/Services/IUserService.cs
@@ -0,0 +1,13 @@
+namespace WebApi.Services;
+
+using WebApi.Entities;
+using WebApi.Models.Users;
+
+public interface IUserService
+{
+    IEnumerable<User> GetAll();
+    User GetById(int id);
+    void Create(CreateRequest model);
+    void Update(int id, UpdateRequest model);
+    void Delete(int id);
+}
